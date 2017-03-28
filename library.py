5695da8629ded11c73bba6905341ca70a32c2fb3
Chapman Shoop <chapman.shoop@gmail.com>
2017-03-27 22:19:31 -0700

    Save PDF

--- library.py
+++ library.py
@@ -6,12 +6,14 @@ https://en.wikipedia.org/wiki/Library_catalog
 import codecs
 from gzip import GzipFile
 import io
+from os.path import expanduser
 
 from kivy.adapters.listadapter import ListAdapter
 from kivy.app import App
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.listview import ListItemButton, ListView
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.textinput import TextInput
 
 from peercoin import call_peercoin_rpc
@@ -22,38 +24,62 @@ HEX_DECODER = codecs.getdecoder("hex_codec")
 BOOKS = [
     {
         "title": "Alice's Adventures in Wonderland",
-        "author": "Lewis Carroll",
+        "authors": ["Lewis Carroll"],
         "transaction_id": "3154d03bcc1f8fbfd89f2c3672567791187c95ba97d55ca05eca2ab4f40c3430",
         "gzipped": False,
         "hex_left_trim": 9,
+        "file_type": "txt",
     },
     {
         "title": "The Narrative of the Life of Frederick Douglass",
-        "author": "Frederick Douglass",
+        "authors": ["Frederick Douglass"],
         "transaction_id": "7c126ee20c5cdc4c8717fe00aa953c402c19b0e3b8a4adac5b848723960a2e4e",
         "gzipped": False,
         "hex_left_trim": 9,
+        "file_type": "txt",
     },
     {
         "title": "Romeo and Juliet",
-        "author": "William Shakespeare",
+        "authors": ["William Shakespeare"],
         "transaction_id": "f40b4c7e97f2e83d1923481c19aed2f6ccaee470c636f7857dc4c970113df204",
         "gzipped": False,
         "hex_left_trim": 9,
+        "file_type": "txt",
     },
     {
         "title": "Through the Looking-Glass",
-        "author": "Lewis Carroll",
+        "authors": ["Lewis Carroll"],
         "transaction_id": "0a440999bafedbdf0e3bdc52f6bdb8caf9634cb38cfef84c8fc2a8ad3f853059",
         "gzipped": False,
         "hex_left_trim": 6,
+        "file_type": "txt",
     },
     {
         "title": "The Emancipation Proclamation",
-        "author": "Abraham Lincoln",
+        "authors": ["Abraham Lincoln"],
         "transaction_id": "2243568d8b9a92d4235503431bc0d2e0c1dfea049f906b66087ab589769fead6",
         "gzipped": True,
         "hex_left_trim": 4,
+        "file_type": "txt",
+    },
+    {
+        "title": "Peercoin White Paper",
+        "authors": [
+            "Sunny King",
+            "Scott Nadal",
+        ],
+        "transaction_id": "1ff7ac2a0d2a87c846fefd57b8b4e8c3fb8ea1ee1d3c7fc9496c7dc407d9f622",
+        "gzipped": False,
+        "hex_left_trim": 9,
+        "file_type": "pdf",
+    },
+    {
+        "title": "The Declaration of Independence",
+        "authors": ["Representatives of the United States of America"],
+        "transaction_id": "6d852d9c3d7208233aa5906f155a602c02f9a0325786b45220cdc919a52eeb9d",
+        "gzipped": True,
+        "hex_left_trim": 4,
+        "file_type": "txt",
     },
 ]
 
@@ -107,44 +133,70 @@ def get_book_text_at_transaction_id(transaction_id, gzipped, hex_left_trim):
     return book_data
 
 
-class MediaViewer(TextInput):
+class MediaScreenManager(ScreenManager):
 
-    PLEASE_SELECT_MESSAGE = "Please select a book from the card catalog."
+    pdf_screen = ObjectProperty()
+    text_screen = ObjectProperty()
 
-    def __init__(self, *args, **kwargs):
-        super(MediaViewer, self).__init__(*args, **kwargs)
-        self.text = self.PLEASE_SELECT_MESSAGE
-
-    def book_changed(self, list_adapter):
-        'Updates Media Veiwer with selected book details and text.'
+    def media_changed(self, list_adapter):
         if len(list_adapter.selection) == 0:
-            self.text = self.PLEASE_SELECT_MESSAGE
+            self.current = self.text_screen.name
+            self.text_screen.text_widget.text = "Welcome to the Library!"
             return
 
         book = BOOKS[list_adapter.selection[0].index]
-        self.text = "{0}\nby {1}\nin peercoin transaction {2}\n\n{3}".format(
-            book["title"],
-            book["author"],
-            book["transaction_id"],
-            get_book_text_at_transaction_id(
+
+        if book["file_type"] == "pdf":
+            self.current = self.pdf_screen.name
+            self.pdf_screen.filename.text = self.pdf_screen.homedir
+            self.pdf_screen.book = book
+        else:
+            self.current = self.text_screen.name
+            tw = self.text_screen.text_widget
+            tw.text = "{0}\nby {1}\nin peercoin transaction {2}\n\n{3}".format(
+                book["title"],
+                ", ".join(book["authors"]),
                 book["transaction_id"],
-                book["gzipped"],
-                book["hex_left_trim"],
+                get_book_text_at_transaction_id(
+                    book["transaction_id"],
+                    book["gzipped"],
+                    book["hex_left_trim"],
+                )
             )
+            tw.cursor = (0, 0) # Scroll back to the top of the text box
+
+
+class PDFScreen(Screen):
+
+    def __init__(self, *args, **kwargs):
+        super(PDFScreen, self).__init__(*args, **kwargs)
+        self.homedir = expanduser("~")
+
+    def save(self, filename):
+        data = get_book_text_at_transaction_id(
+            self.book["transaction_id"],
+            self.book["gzipped"],
+            self.book["hex_left_trim"],
         )
-        self.cursor = (0, 0) # Scroll back to the top of the text box
+
+        with open(filename, 'wb') as pdf_file:
+            pdf_file.write(data)
+
+
+class TextScreen(Screen):
+    pass
 
 
 class LibraryBrowser(BoxLayout):
 
     card_catalog = ObjectProperty()
-    media_viewer = ObjectProperty()
+    media_screen_manager = ObjectProperty()
 
     def __init__(self, *args, **kwargs):
         super(LibraryBrowser, self).__init__(*args, **kwargs)
 
         self.card_catalog.adapter.bind(
-            on_selection_change=self.media_viewer.book_changed
+            on_selection_change=self.media_screen_manager.media_changed
         )
 
 
